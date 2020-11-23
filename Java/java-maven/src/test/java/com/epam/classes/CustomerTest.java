b27546e3b090bdee0de9727fa78e5e78ca62128e
sagarikabandari10 <sagarika.padakanti10@gmail.com>
2020-11-23 16:12:21 +0300

    changed test to junit.

--- Java/java-maven/src/test/java/com/epam/classes/CustomerTest.java
+++ Java/java-maven/src/test/java/com/epam/classes/CustomerTest.java
@@ -1,31 +1,30 @@
 package com.epam.classes;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class CustomerTest {
+public class CustomerTest {
 
     @Test
-    void isCreditCardWithInRangeIsValid() {
+    public void isCreditCardWithInRangeIsValid() {
         long minRange = 5000, maxRange = 80000, creditCardNumber = 60000;
-        assertTrue(Customer.isCreditCardWithInRange(minRange, maxRange, creditCardNumber));
+        Assert.assertTrue(Customer.isCreditCardWithInRange(minRange, maxRange, creditCardNumber));
     }
 
     @Test
-    void isCreditCardWithInRangeIsInvalid() {
+    public void isCreditCardWithInRangeIsInvalid() {
         long minRange = 5000, maxRange = 80000, creditCardNumber = 600;
-        assertFalse(Customer.isCreditCardWithInRange(minRange, maxRange, creditCardNumber));
+        Assert.assertFalse(Customer.isCreditCardWithInRange(minRange, maxRange, creditCardNumber));
     }
 
-    @Test
-    void isCreditCardWithInRangeMaxGreaterThanMinValue() {
+    @Test(expected = IllegalArgumentException.class)
+    public void isCreditCardWithInRangeMaxGreaterThanMinValue() {
         long minRange = 500, maxRange = 80, creditCardNumber = 100;
-        assertThrows(IllegalArgumentException.class, () -> Customer.isCreditCardWithInRange(minRange, maxRange, creditCardNumber));
+        Customer.isCreditCardWithInRange(minRange, maxRange, creditCardNumber);
     }
 
     @Test
-    void getCustomers() {
-        assertTrue(Customer.getCustomers().size() > 0, "Customer should not be empty.");
+    public void getCustomers() {
+        Assert.assertTrue(Customer.getCustomers().size() > 0);
     }
 }
