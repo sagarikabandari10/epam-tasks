68e51aa5a4095a96bf184c6ef4bc9de98fe004db
sagarikabandari10 <sagarika.padakanti10@gmail.com>
2020-11-25 14:44:13 +0300

    Jenkins Task Artifacts generated for AirportTest.

--- Java/java-maven/src/test/java/com/epam/classes/StudentTest.java
+++ Java/java-maven/src/test/java/com/epam/classes/StudentTest.java
@@ -1,49 +1,41 @@
 package com.epam.classes;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class StudentTest {
+public class StudentTest {
 
     @Test
-    void getStudentsWithValidYear() {
+    public void getStudentsWithValidYear() {
         List<Student> students = Student.getStudents();
         int year = 1986;
-        assertTrue(Student.getStudentsByYear(students, year).size() > 0);
+        Assert.assertTrue(Student.getStudentsByYear(students, year).size() > 0);
     }
 
-    @Test
-    void getStudentsByYearWithNullStudentParameter() {
+    @Test(expected = IllegalArgumentException.class)
+    public void getStudentsByYearWithNullStudentParameter() {
         int year = 1986;
-        assertThrows(IllegalArgumentException.class, () -> Student.getStudentsByYear(null, year));
+        Student.getStudentsByYear(null, year);
     }
 
     @Test
-    void getStudentsByYearWithNoStudent() {
-        int year = 1986;
-        assertThrows(IllegalArgumentException.class, () -> Student.getStudentsByYear(new ArrayList<>(), year));
-    }
-
-    @Test
-    void getStudentsWithInvalidYear() {
+    public void getStudentsWithInvalidYear() {
         List<Student> students = Student.getStudents();
         int year = 2020;
-        assertFalse(Student.getStudentsByYear(students, year).size() > 0);
+        Assert.assertFalse(Student.getStudentsByYear(students, year).size() > 0);
     }
 
-    @Test
-    void getStudentsByYearThrowsException() {
+    @Test(expected = IllegalArgumentException.class)
+    public void getStudentsByYearThrowsException() {
         List<Student> students = Student.getStudents();
         int year = 123;
-        assertThrows(IllegalArgumentException.class, () -> Student.getStudentsByYear(students, year));
+        Student.getStudentsByYear(students, year);
     }
 
     @Test
-    void getStudents() {
-        assertTrue(Student.getStudents().size() > 0, "Student should not be empty.");
+    public void getStudents() {
+        Assert.assertTrue(Student.getStudents().size() > 0);
     }
 }
